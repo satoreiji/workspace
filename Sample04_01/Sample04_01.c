6291888684b616b5dc9ee05f9f87629ad804de53
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:31:14 +0900

    Sample04_01を追加

---
+++ Sample04_01/Sample04_01.c
@@ -0,0 +1,16 @@
+/*
+ * Sample04_01.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+#include <stdio.h>
+
+int main(void){
+    printf("2＋3は%dです。\n", 2 + 3);
+
+    printf("2×3は%dです。\n", 2 * 3);
+
+    return 0;
+}
