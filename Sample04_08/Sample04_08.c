dbce22c71dcbae8839c88642e4106c5bc7a63af8
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:49:22 +0900

    Sample04_08を追加

---
+++ Sample04_08/Sample04_08.c
@@ -0,0 +1,27 @@
+/*
+ * Sample04_08.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+#include <stdio.h>
+
+int main(void){
+
+    int a = 15;
+    int b = 7;
+    int c = 3;
+    int sum = 0;
+
+    sum += a;
+    printf("aまで足した結果は %d\n", sum);
+
+    sum += b;
+    printf("bまで足した結果は %d\n", sum);
+
+    sum += c;
+    printf("cまで足した結果は %d\n", sum);
+
+    return 0;
+}
+
