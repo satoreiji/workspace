53ee7db113036aa14d281d0d3f1dfba788327923
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:51:02 +0900

    Sample04_13を追加

---
+++ Sample04_13/Sample04_13.c
@@ -0,0 +1,19 @@
+/*
+ * Sample04_13.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+
+    int inumber = 5;
+    double dnumber = 30.48;
+
+    printf("%dフィートは約%fセンチメートルです\n", inumber, inumber * dnumber);
+
+    return 0;
+}
