9b12acfee21f514c71c7ca8df7d85e32219a24e7
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:44:02 +0900

    Sample04_02を追加

---
+++ Sample04_02/Sample04_02.c
@@ -0,0 +1,23 @@
+/*
+ * Sample04_02.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+
+    int number1 = 2;
+    int number2 = 3;
+
+    printf("%d＋%dは%dです。\n", number1, number2, number1 + number2);
+
+    number1 = number1 + 1;
+
+    printf("number1の値に1を足すと%dとなります。\n", number1);
+
+    return 0;
+}
