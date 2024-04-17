a874dc073b0559e8c30232b144d3d24c7019fd8f
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:45:29 +0900

    Sample04_04を追加

---
+++ Sample04_04/Sample04_04.c
@@ -0,0 +1,18 @@
+/*
+ * Sample04_04.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+
+    int number1 = 3;
+
+    printf("(number1 = number1 + 5)の値は%dです。\n", number1 = number1 + 5);
+
+    return 0;
+}
