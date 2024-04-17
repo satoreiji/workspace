53656ca26f69c685adbaac31c467aa554e470757
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:47:18 +0900

    Sample04_5を追加

---
+++ Sample04_05/Sample04_-5.c
@@ -0,0 +1,23 @@
+/*
+ * Sample04_-5.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+#include <stdio.h>
+
+int main(void){
+
+    int number1 = 23;
+    int number2 = 10;
+
+    printf("算術演算子の練習です\n");
+    printf("%d + %d = %d\n",  number1, number2, number1 + number2);
+    printf("%d - %d = %d\n",  number1, number2, number1 - number2);
+    printf("%d * %d = %d\n",  number1, number2, number1 * number2);
+    printf("%d / %d = %d\n",  number1, number2, number1 / number2);
+    printf("%d %% %d = %d\n", number1, number2, number1 % number2);
+
+    return 0;
+}
