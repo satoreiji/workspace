47fb191feec26d50cd15ac164eba6e8af1f2af3b
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:51:37 +0900

    Sample04_14を追加

---
+++ Sample04_14/Sample04_14.c
@@ -0,0 +1,20 @@
+/*
+ * Sample04_14.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+
+    int number1 = 10;
+    int number2 = 4;
+    double div = number1 / number2;
+
+    printf("%dメートルの%d分の1は%fメートルです\n", number1, number2, div);
+
+    return 0;
+}
