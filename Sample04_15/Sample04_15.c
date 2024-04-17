d7e1ef6dd8295fec2c40a597027d4f3c5ba8cfa5
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-17 16:51:50 +0900

    Sample04_15を追加

---
+++ Sample04_15/Sample04_15.c
@@ -0,0 +1,24 @@
+/*
+ * Sample04_15.c
+ *
+ *  Created on: 2024/04/17
+ *      Author: User
+ */
+
+#include <stdio.h>
+
+int main(void){
+
+    int a = 3;
+    int b = 4;
+    int c = 5;
+    int d;
+    int e;
+
+    d = a + b;
+    e = c * d;
+
+    printf("演算結果は %d です。\n", e);
+
+    return 0;
+}
