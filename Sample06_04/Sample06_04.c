3b822e29cb3925b52ca2912f63ecaf2db98baeff
satoreiji <sato.reiji1223@alphatc.co.jp>
2024-04-18 18:04:42 +0900

    Sample06_04を追加

---
+++ Sample06_04/Sample06_04.c
@@ -0,0 +1,48 @@
+/*
+ * Sample06_04.c
+ *
+ *  Created on: 2024/04/18
+ *      Author: User
+ */
+
+/*
+ * Sample06_02.c
+ *
+ *  Created on: 2024/04/18
+ *      Author: User
+ */
+#include <stdio.h>
+
+int main(void){
+
+	int first;
+	int end;
+	int i;
+	int sum=0;
+	int sump=0;
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+	printf("最初の数を入力\n");
+	scanf("%d",&first);
+
+	printf("最後の数を入力\n");
+	scanf("%d", &end);
+
+	if(first > end){
+		printf("最初の数の法が最後の数より大きいです.\n");
+
+	}else{
+        i = first;
+        while(i <= end){
+            sump = sum;
+            sum += i;
+            printf("%d + %d = %d\n", sump, i, sum);
+            i++;
+		}
+		printf("%dから%dまでの和は%dです\n",first,end,sum);
+	}
+	return 0;
+
+}
+
+
